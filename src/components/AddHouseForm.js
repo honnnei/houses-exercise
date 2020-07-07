f42a179acaca0bde781478f73a7267e5148d5e39
honnnei <hannpop@gmail.com>
2020-07-07 14:19:37 +0200

    add house form and axios post work

---
+++ src/components/AddHouseForm.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+export default function AddHouseForm() {
+    const [houseAddress, setHouseAddress] = useState("");
+    const [houseOwner, setHouseOwner] = useState("");
+    const [housePrice, setHousePrice] = useState("");
+    const [houseArea, setHouseArea] = useState(0);
+
+    const addHouse = (event) => {
+        event.preventDefault();
+        console.log(houseAddress, houseOwner, housePrice, houseArea);
+
+        Axios.post('http://mr-test-backend.sadek.usermd.net/houses', {
+           address: houseAddress,
+           owner: houseOwner,
+           price: housePrice,
+           area: houseArea
+
+          })
+            .then((response) => {
+            //   window.location.reload(false);
+            console.log(response);
+            }).catch((error) => {
+              console.log(error);
+            });
+
+    }
+
+    return (
+        <div className="addhouseform-container">
+            <form>
+                <label>Adres</label>
+                <input
+                type="text"
+                value={houseAddress}
+                onChange={(e) => setHouseAddress(e.target.value)}
+                />
+                <label>Właściciel</label>
+                <input
+                type="text"
+                value={houseOwner}
+                onChange={(e) => setHouseOwner(e.target.value)}
+                />
+                <label>Cena</label>
+                <input
+                type="text"
+                value={housePrice}
+                onChange={(e) => setHousePrice(e.target.value)}
+                />
+                <label>Powierzchnia</label>
+                <input
+                type="number"
+                value={houseArea}
+                onChange={(e) => setHouseArea(e.target.value)}
+                />
+                <button onClick={addHouse}>Dodaj Dom</button>
+            </form>
+            
+        </div>
+    )
+}
---
+++ src/components/Offer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function Offer() {
+    return (
+        <div>
+            <Link to='/dodaj-dom'>Dodaj Dom</Link>
+        </div>
+    )
+}
