536e7eb9b659fe3d48046df116ca4bb9e8ec48c2
honnnei <hannpop@gmail.com>
2020-07-07 16:59:12 +0200

    styling

--- src/components/AboutUsCard.js
+++ src/components/AboutUsCard.js
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 export default function AboutUsCard() {
     return (
         <div className="card about-us-card">
-            <h1>Card</h1>
-            <Link to='/o-nas'>Zobacz Wiecej</Link>
+            <h1 className="title">Kilka Słów O Nas</h1>
+            <h1 className="subtitle">Kim Jesteśmy i Dokąd Zmierzamy</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <button><Link to='/o-nas' className="subtitle">Zobacz Wiecej</Link></button>
         </div>
     )
 }
