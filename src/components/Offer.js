d1d7b4f62069a49f3b3ae5f76bef51ef5795ced5
honnnei <hannpop@gmail.com>
2020-07-07 14:27:10 +0200

    get all houses route

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import { Link } from "react-router-dom";
 
 export default function Offer() {
+
+    const [houseData, setHouseData] = useState([]);
+
+    const getHouseData = () => {
+        Axios.get('http://mr-test-backend.sadek.usermd.net/houses')
+            .then((response) => {
+            //   window.location.reload(false);
+            console.log(response.data);
+            setHouseData(response.data.houses)
+            }).catch((error) => {
+              console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        getHouseData();
+      }, []);
+
     return (
         <div>
             <Link to='/dodaj-dom'>Dodaj Dom</Link>
+            {houseData ? houseData.map(house => (
+                <div>
+                    <h3>{house.address}</h3>
+                    <h3>{house.price}</h3>
+                </div>
+            )) : ""}
+
         </div>
     )
 }
