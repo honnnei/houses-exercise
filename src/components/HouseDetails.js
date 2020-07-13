56e4b317e97a5d26ef03bc6f651d3680d2e17e67
honnnei <hannpop@gmail.com>
2020-07-13 17:50:42 +0200

    logic improvements done

--- src/components/HouseDetails.js
+++ src/components/HouseDetails.js
@@ -5,6 +5,7 @@ import { deleteHouse } from '../api';
 
 export default function HouseDetails(props) {
     const [house, setHouse ] = useState(props.location.state.houseData)
+    const { address, owner, price, area } = house;
     
     let history = useHistory();
 
@@ -18,13 +19,13 @@ export default function HouseDetails(props) {
     return (
         <div>
             <h3>Adres</h3>
-            <h4>{house.address}</h4>
+            <h4>{address}</h4>
             <h3>Właściciel</h3>
-            <h4>{house.owner}</h4>
+            <h4>{owner}</h4>
             <h3>Cena</h3>
-            <h4>{house.price}</h4>
+            <h4>{price}</h4>
             <h3>Powierzchnia</h3>
-            <h4>{house.area}</h4>
+            <h4>{area}</h4>
             <button onClick={() => deleteHouseCall(house._id)}>Usuń</button>
         </div>
     )
