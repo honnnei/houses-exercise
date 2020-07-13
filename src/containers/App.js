c934c5b088eb585fc14d9b31eac78b005a395e0c
honnnei <hannpop@gmail.com>
2020-07-13 17:43:23 +0200

    router component

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,19 +6,12 @@ import AboutUsPage from './AboutUsPage';
 import OfferPage from './OfferPage';
 import HouseDetails from '../components/HouseDetails';
 import AddHouseFormPage from './AddHouseFormPage';
+import Router from './Router';
 
 function App() {
   return (
     <div className="app-container">
-      <BrowserRouter >
-      <Switch>
-            <Route id="homepage" path='/' exact component={Homepage}/>
-            <Route id="about_us" path='/o-nas' exact component={AboutUsPage}/>
-            <Route id="offer" path='/oferta' exact component={OfferPage}/>
-            <Route id="details" path='/dom' exact component={HouseDetails}/>
-            <Route id="add_house" path='/dodaj-dom' exact component={AddHouseFormPage}/>
-          </Switch>
-      </BrowserRouter>
+      <Router />
     </div>
   );
 }
